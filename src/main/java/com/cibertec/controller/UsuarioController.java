58bb7bf10993f47b51b4ea84e49562dcc894dc99
Diego Esquivel <dael200414@gmail.com>
2022-06-11 16:19:31 -0500

    LOGIN-PROCESO

---
+++ src/main/java/com/cibertec/controller/UsuarioController.java
@@ -0,0 +1,58 @@
+package com.cibertec.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.cibertec.entity.Usuario;
+import com.cibertec.repository.IUsuarioRepository;
+
+/**
+ * @author dael2
+ *
+ */
+@Controller
+public class UsuarioController {
+	@Autowired
+	private IUsuarioRepository repou;
+
+	@GetMapping("/login/cargar")
+	public String cargarform(Model model) {
+		model.addAttribute("usuario", new Usuario());
+		return "Login";
+	}
+
+	@PostMapping("/login/validar")
+	public String validarIngreso(@ModelAttribute Usuario usuario, Model model) {
+		//System.out.println("Del Login: " + usuario); // correo y clave
+		Usuario u = repou.findByCorreoAndClave(usuario.getCorreo(), usuario.getClave());
+		//System.out.println("Del Find(Busqueda): " + u);
+		if (u == null) {
+			model.addAttribute("error", "Usuario o Clave Incorrecto");
+			return "Login";
+		}
+		model.addAttribute("usuario", u);
+		return "Principal";
+	}
+
+	@GetMapping("/usuario/cargar")
+	public String cargarformRegistro(Model model) {
+		model.addAttribute("usuario", new Usuario());
+		return "Registro";
+	}
+
+	@PostMapping("/usuario/grabar")
+	public String grabarRegistro(@ModelAttribute Usuario usuario, Model model) {
+		System.out.println(usuario);
+		try {
+			repou.save(usuario);
+			model.addAttribute("success", "Registro exitoso");
+		} catch (Exception e) {
+			model.addAttribute("error", "Error al regitrar " + e.getMessage());
+		}
+		return "Registro";
+	}
+}
--- src/main/java/com/cibertec/entity/Usuario.java
+++ src/main/java/com/cibertec/entity/Usuario.java
@@ -1,5 +1,31 @@
 package com.cibertec.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "usuario")
+@Data
 public class Usuario {
-private int id;
+	@Id
+	@Column(name = "id")
+	private int id;
+	@Column(name = "nombre")
+	private String nombre;
+	@Column(name = "apellido")
+	private String apellido;
+	@Column(name = "username")
+	private String correo;
+	@Column(name = "contraseña")
+	private String clave;
+	@Column(name = "fecNac")
+	private String fechaNacimiento;
+	@Column(name = "idrol")
+	private int idrol;
+	@Column(name = "idestado")
+	private int idestado;
 }
