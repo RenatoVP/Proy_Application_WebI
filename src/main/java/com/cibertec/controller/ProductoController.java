bc20abb5cbd6363248bd10ddad93826638667078
renato <renatovp32@gmail.com>
2022-06-26 00:44:20 -0500

    Se creo reporte grafico de productos y se actualizo consulta productos, ahora se consulta por nombre y categoria, ademas se agrego jaspersoft al proyecto

--- src/main/java/com/cibertec/controller/ProductoController.java
+++ src/main/java/com/cibertec/controller/ProductoController.java
@@ -1,8 +1,15 @@
 package com.cibertec.controller;
 
+import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -18,6 +25,10 @@ import com.cibertec.repository.ICategoriaRepository;
 import com.cibertec.repository.IEstadoRepository;
 import com.cibertec.repository.IProductoRepository;
 
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
 @Controller
 @RequestMapping("/Producto")
 public class ProductoController {
@@ -31,6 +42,12 @@ public class ProductoController {
 	@Autowired
 	private IEstadoRepository EstadoRepo;
 	
+	@Autowired
+	private DataSource datasource;
+	
+	@Autowired
+	private ResourceLoader resourceLoader;
+	
 	@GetMapping("/Listar")
 	public String listaProductos(Model model) {
 		model.addAttribute("lstProducto", ProductoRepo.findAll());
@@ -39,19 +56,20 @@ public class ProductoController {
 	
 	@GetMapping("/Consulta")
 	public String consultaProductoForm(Model model) {
+		model.addAttribute("lstCategoria", CategoriaRepo.findAll());
 		return "Producto/consultaProductos";
 	}
 	
 	@PostMapping("/Consulta")
-	public String consultaProducto(@RequestParam String nomProducto, Model model) {
+	public String consultaProducto(@RequestParam String nomProducto, @RequestParam int idCategoria, Model model) {
 		List<Producto> lstProductos;
 		
-		if(nomProducto.equals("")) {
-			lstProductos = ProductoRepo.findAll();
-		}else {
+		if(idCategoria == -1)
 			lstProductos = ProductoRepo.findAllByProductName(nomProducto);
-		}
+		else
+			lstProductos = ProductoRepo.findAllByProductNameAndCategory(nomProducto, idCategoria);
 		
+		model.addAttribute("lstCategoria", CategoriaRepo.findAll());
 		model.addAttribute("lstProducto", lstProductos);
 		return "Producto/consultaProductos";
 	}
@@ -82,5 +100,44 @@ public class ProductoController {
 	public String elimina(@ModelAttribute Producto producto, Model model) {
 		ProductoRepo.delete(producto);
 		return "redirect:/Producto/Listar";
+	}
+	
+	/*@GetMapping("/Reporte")
+	public void reporte(@RequestParam int idCategoria, HttpServletResponse response) {
+		response.setHeader("Content-Disposition", "attachment; filename=\"productosx-" + idCategoria + ".pdf\";");
+		response.setHeader("Cache-Control", "no-cache");
+		response.setHeader("Pragma", "no-cache");
+		response.setDateHeader("Expires", 0);
+		response.setContentType("application/pdf");
+		
+		try {
+			Map<String, Object> param = new HashMap<String, Object>();
+			param.put("pIdCategoria", idCategoria);
+			
+			String ru = resourceLoader.getResource("classpath:productosxCategoria.jasper").getURI().getPath();
+			JasperPrint jasperPrint = JasperFillManager.fillReport(ru, param, datasource.getConnection());
+			OutputStream outStream = response.getOutputStream();
+			JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}*/
+	
+	@GetMapping("/Grafico")
+	public void grafico(HttpServletResponse response) {
+		response.setHeader("Content-Disposition", "attachment; filename=\"grafico-productos.pdf\";");
+		response.setHeader("Cache-Control", "no-cache");
+		response.setHeader("Pragma", "no-cache");
+		response.setDateHeader("Expires", 0);
+		response.setContentType("application/pdf");
+		
+		try {
+			String ru = resourceLoader.getResource("classpath:graficoProductos.jasper").getURI().getPath();
+			JasperPrint jasperPrint = JasperFillManager.fillReport(ru, null, datasource.getConnection());
+			OutputStream outStream = response.getOutputStream();
+			JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/com/cibertec/repository/IProductoRepository.java
+++ src/main/java/com/cibertec/repository/IProductoRepository.java
@@ -12,4 +12,7 @@ import com.cibertec.entity.Producto;
 public interface IProductoRepository extends JpaRepository<Producto, String>{
 	@Query("select p from Producto p where p.descripcion like %?1%")
 	public List<Producto> findAllByProductName(String productName);
+	
+	@Query("select p from Producto p where p.descripcion like %?1% and p.idcategoria = ?2")
+	public List<Producto> findAllByProductNameAndCategory(String productName, int idCategoria);
 }
