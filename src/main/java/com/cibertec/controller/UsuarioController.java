70a079519ba5ee36f05c652278351a809e81c9d3
renato <renatovp32@gmail.com>
2022-06-26 13:53:17 -0500

    Se agrego listado y registro de productos (Diego Esquivel)

--- src/main/java/com/cibertec/controller/UsuarioController.java
+++ src/main/java/com/cibertec/controller/UsuarioController.java
@@ -6,8 +6,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.cibertec.entity.Usuario;
+import com.cibertec.repository.IEstadoRepository;
+import com.cibertec.repository.IRolRepository;
 import com.cibertec.repository.IUsuarioRepository;
 
 /**
@@ -18,6 +21,10 @@ import com.cibertec.repository.IUsuarioRepository;
 public class UsuarioController {
 	@Autowired
 	private IUsuarioRepository repou;
+	@Autowired
+	private IEstadoRepository repoe;
+	@Autowired
+	private IRolRepository repor;
 
 	@GetMapping("/login/cargar")
 	public String cargarform(Model model) {
@@ -37,22 +44,31 @@ public class UsuarioController {
 		model.addAttribute("usuario", u);
 		return "redirect:/Dashboard";
 	}
-
+	
+	//Listar
+	@GetMapping("/usuario/lista")
+	public String listar(Model model) {
+		model.addAttribute("lstusuario", repou.findAll());
+		return "Usuario/listaUsuario";
+	}
+	
+	//Registrar
 	@GetMapping("/usuario/cargar")
 	public String cargarformRegistro(Model model) {
 		model.addAttribute("usuario", new Usuario());
+		model.addAttribute("lstrol", repor.findAll());
+		model.addAttribute("lstestado", repoe.findAll());
 		return "Usuario/Registro";
 	}
 
 	@PostMapping("/usuario/grabar")
-	public String grabarRegistro(@ModelAttribute Usuario usuario, Model model) {
-		System.out.println(usuario);
+	public String grabarRegistro(@ModelAttribute Usuario usuario, RedirectAttributes redirect) {
 		try {
 			repou.save(usuario);
-			model.addAttribute("success", "Registro exitoso");
+			redirect.addFlashAttribute("success", "Registro exitoso");
 		} catch (Exception e) {
-			model.addAttribute("error", "Error al regitrar " + e.getMessage());
+			redirect.addFlashAttribute("error", "Error al regitrar " + e.getMessage());
 		}
-		return "Registro";
+		return "redirect:/usuario/lista";
 	}
 }
