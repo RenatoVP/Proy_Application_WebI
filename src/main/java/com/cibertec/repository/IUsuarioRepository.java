75d1489e61543cb1a43b5e46fb4dbda4bdfd28ce
FabiJR <fabjr2000@gmail.com>
2022-06-26 19:15:13 -0500

    Usuario Finalizado

--- src/main/java/com/cibertec/repository/IUsuarioRepository.java
+++ src/main/java/com/cibertec/repository/IUsuarioRepository.java
@@ -3,6 +3,7 @@ package com.cibertec.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.cibertec.entity.Usuario;
@@ -13,4 +14,7 @@ public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
 	Usuario findByCorreoAndClave(String correo, String clave);
 	
 	List<Usuario> findAllByIdrol(int idrol);
+	
+	@Query("select u from Usuario u where u.nombre like %?1%")
+	public List<Usuario> findAllByUserName(String usuname);
 }
